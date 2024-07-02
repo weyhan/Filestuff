3bddd2dd6c68b809816ff5622cd0939819bda5e5
weyhan <weyhan@motileware.com>
2024-07-02 22:27:00 +0800

    Initial Commit

---
+++ Filestuff/Filestuff.h
@@ -0,0 +1,18 @@
+//
+//  Filestuff.h
+//  Filestuff
+//
+//  Created by WeyHan Ng on 02/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Filestuff.
+FOUNDATION_EXPORT double FilestuffVersionNumber;
+
+//! Project version string for Filestuff.
+FOUNDATION_EXPORT const unsigned char FilestuffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Filestuff/PublicHeader.h>
+
+
